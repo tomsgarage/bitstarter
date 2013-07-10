c7d96a0875cb905f4799b9bbdb9ee715a6be956f
tomsgarage <tomsgarage1@gmail.com>
2013-07-10 10:29:40 -0400

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFileSync("index.html");
+  var outputString = buf.toString();
+  response.send(outputString);
 });
 
 var port = process.env.PORT || 5000;
